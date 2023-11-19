73c9a481cb53a8eb27d39451d5e2e0222c3fc437
Noora Sassali <noora.sassali@tuni.fi>
2023-11-19 19:47:40 +0200

    Updated test suite for toNumber function

--- test/toNumber.test.js
+++ test/toNumber.test.js
@@ -3,9 +3,64 @@
  */
 
 /**
- * Tests the toNumber function.
+ * Test suite for toNumber function.
  */
 
 import toNumber from '../src/toNumber';
 
-// placeholder for tests
+describe("toNumber", () => {
+  // constant values for Number minimum and maximum
+  const maxValue = 1.7976931348623157e+308;
+  const minValue = 5e-324; 
+
+  // basics 
+  test("returns converted number as a Number object", () => {
+    expect(toNumber(2)).toBe(Number(2));
+  })
+  test("returns correct value, when Number literal MIN_VALUE is used", () => {
+    expect(toNumber(Number.MAX_VALUE)).toBe(Number(maxValue));
+  })
+  test("returns correct value, when Number literal MAX_VALUE is used", () => {
+    expect(toNumber(Number.MIN_VALUE)).toBe(Number(minValue));
+  })
+
+
+  // input as string values 
+  test("returns converted number when the input is number in string format", () => {
+    expect(toNumber("2")).toBe(Number(2)); 
+  })
+  test("returns correct value for string infinity", () => {
+    expect(toNumber("Infinity")).toBe(Number(Infinity));
+  })
+  test("returns correct value for string minus infinity", () => {
+    expect(toNumber("-Infinity")).toBe(Number(Infinity));
+  })
+
+
+  // over and underflows, infinity literal
+  test("returns correct value for infinity", () => {
+    expect(toNumber(Infinity)).toBe(Number(Infinity));
+  })
+  test("returns correct value for minus infinity", () => {
+    expect(toNumber(Infinity)).toBe(Number(Infinity));
+  })
+  test("returns infinity for number larger than maximum value", () => {
+    expect(toNumber(maxValue + 2)).toBe(Number(Infinity));
+  })
+  test("returns minus infinity for number smaller than minimum value", () => {
+    expect(toNumber(-maxValue -2)).toBe(Number(-Infinity));
+  })
+
+  // non-convertible inputs 
+  test("returns NaN for non-convertible string", () => {
+    expect(toNumber("3.3op")).toBe(Number(NaN));
+  })
+  test("returns NaN for non-convertible object", () => {
+    const nonConvertibleObject = {firstDigit:2, secondDigit:3};
+    expect(toNumber(nonConvertibleObject)).toBe(Number(NaN));
+  })
+  test("returns NaN for undefined", () => {
+    expect(toNumber(undefined)).toBe(Number(NaN)); 
+  })
+
+})
