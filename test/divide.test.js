15c47e284f903a52226511122aeaf946d7357983
Noora Sassali <noora.sassali@tuni.fi>
2023-12-04 22:23:35 +0200

    Added some cases to test/divide.test.js

--- test/divide.test.js
+++ test/divide.test.js
@@ -15,6 +15,13 @@ describe("divide", () => {
   test("dividing positive number with negative number returns a negative result", () => {
     expect(divide(6, -2)).toBe(-3);
   })
+  test("dividing negative number with positive number returns a negative result", () => {
+    expect(divide(-10, 5)).toBe(-2);
+  })
+  test("dividing negative number with negative number returns a positive result", () => {
+    expect(divide(-8, -4)).toBe(2);
+  })
+
   test("dividing integers when result is not an integer", () => {
     expect(divide(10, 3)).toBeCloseTo(10/3, 3);
   })
@@ -24,6 +31,21 @@ describe("divide", () => {
   test("dividing negative number by zero returns minus infinity", () => {
     expect(divide(-1, 0)).toBe(-Infinity);
   })
+
+  // Dividend or divisor is (-)Infinity 
+  test("dividing positive number with Infinity, results in zero", () => {
+    expect(divide(2,Infinity)).toBe(0);
+  });
+  test("dividing negative number with Infinity, results in zero", () => {
+    expect(divide(-2,Infinity)).toBe(0);
+  });
+  test("dividing Infinity with positive number, results in Infinity", () => {
+    expect(divide(Infinity,2)).toBe(Infinity);
+  });
+  test("dividing -Infinity with positive number, results in -Infinity", () => {
+    expect(divide(-Infinity,2)).toBe(-Infinity);
+  });
+
   test("dividing zero by zero returns NaN", () => {
     expect(divide(0, 0)).toBeNaN();
   })
