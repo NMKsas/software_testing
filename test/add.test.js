4947697cb488bcd9f2b9476e5e65ff20fdaff8e0
Noora Sassali <noora.sassali@tuni.fi>
2023-12-04 21:43:22 +0200

    Added infinity test cases to add.test.js

--- test/add.test.js
+++ test/add.test.js
@@ -9,16 +9,40 @@
 import add from '../src/add';
 
 describe("add", () => {
-  test("6 + 4 should equal 10", () => {
+  test("should add two positive numbers: 6 + 4 should equal 10", () => {
     expect(add(6, 4)).toBe(10);
   })
-  test("1 - 3 should equal -2", () => {
+  test("should add a positive and a negative number: 1 - 3 should equal -2", () => {
     expect(add(1, -3)).toBe(-2);
   })
-  test("-5 - 4 should equal -9", () => {
+  test("should add two negative numbers: -5 - 4 should equal -9", () => {
     expect(add(-5, -4)).toBe(-9);
   })
-  test("-2 + 8 should equal 6", () => {
+  test("should add a negative and a positive number: -2 + 8 should equal 6", () => {
     expect(add(-2, 8)).toBe(6);
   })
-})
+
+
+  test("should result in Infinity, when positive number is added to Number.MAX_VALUE", () => {
+    expect(add(Number.MAX_VALUE,100)).toBe(Infinity);
+  });
+  test("should result in -Infinity, when negative number is added to Number.MIN_VALUE", () => {
+    expect(add(Number.MIN_VALUE,-100)).toBe(-Infinity);
+  });
+
+
+  test("should result in Infinity, when positive number is added to Infinity", () => {
+    expect(add(Infinity,1)).toBe(Infinity);
+  });
+  test("should result in Infinity, when negative number is added to Infinity", () => {
+    expect(add(Infinity,-100000)).toBe(Infinity);
+  });
+  test("should result in -Infinity, when positive number is added to -Infinity", () => {
+    expect(add(-Infinity,1)).toBe(-Infinity);
+  });
+  test("should result in -Infinity, when negative number is added to -Infinity", () => {
+    expect(add(-Infinity,-100000)).toBe(-Infinity);
+  });
+
+}); 
+
