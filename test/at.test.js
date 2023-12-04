6c4b66dc439e8216eb04717a4fcf94a5a08823f3
Noora Sassali <noora.sassali@tuni.fi>
2023-12-04 22:00:56 +0200

    Added two test cases that use single string instead of string array as path

--- test/at.test.js
+++ test/at.test.js
@@ -11,19 +11,28 @@ import at from '../src/at';
 const object = { 'a': [{ 'b': { 'c': 3 } }, 4] }
 
 describe("at", () => {
-  test("return multiple values as an array", () => {
+  test("given string path, returns root value as an array", () => {
+    expect(at(object,'a')).toStrictEqual([[{ 'b': { 'c': 3 } }, 4]]);
+  })
+  test("given string array path, returns root value as an array", () => {
+    expect(at(object,['a'])).toStrictEqual([[{ 'b': { 'c': 3 } }, 4]]);
+  })
+  test("given string array path, returns multiple values as an array", () => {
     expect(at(object, ['a[0].b.c', 'a[1]'])).toStrictEqual([3, 4]);
   })
-  test("return only one value in an array", () => {
+  test("given string array path, returns only one value in an array", () => {
     expect(at(object, ['a[0].b'])).toStrictEqual([{'c': 3}]);
   })
-  test("return undefined when search path is incorrect", () => {
+  test("given string path, returns undefined when search path is incorrect", () => {
+    expect(at(object, 'b')).toStrictEqual([undefined]);
+  })
+  test("given string array path, returns undefined when search path is incorrect", () => {
     expect(at(object, ['b'])).toStrictEqual([undefined]);
   })
-  test("returned array can contain undefined and defined values", () => {
+  test("returns an array that can contain undefined and defined values", () => {
     expect(at(object, ['d', 'a[0].b.c', 'a[1]'])).toStrictEqual([undefined, 3, 4]);
   })
-  test("return undefined when object is undefined", () => {
+  test("returns undefined when object is undefined", () => {
     expect(at(undefined, ['a'])).toStrictEqual([undefined]);
   })
 })
